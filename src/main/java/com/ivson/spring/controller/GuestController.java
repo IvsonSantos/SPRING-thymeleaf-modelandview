4d4c4379f546f29097391cf8066984c16b05a9c7
Ivson Santos <santosivson@hotmail.com>
2018-06-16 13:22:18 -0300

    Projeto criado

--- src/main/java/com/ivson/spring/controller/GuestController.java
+++ src/main/java/com/ivson/spring/controller/GuestController.java
@@ -1,14 +1,29 @@
 package com.ivson.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ivson.spring.repository.GuestRepository;
 
 @Controller
 public class GuestController {
 
+	@Autowired
+	GuestRepository guestRepository;
+	
+	/**
+	 * podemos retornar um objeto do tipo ModelAndView, que nos permite, além de
+	 * informar o nome da view, adicionar objetos para serem usados no HTML.
+	 * 
+	 * @return
+	 */
 	@GetMapping("/guest")
-	public String getAll() {
-		return "ListaConvidados";
+	public ModelAndView listAll() {
+		ModelAndView modelAndView = new ModelAndView("GuestList");
+		modelAndView.addObject("guests", guestRepository.findAll());
+		return modelAndView;
 	}
 
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into guest (id, nome, quantidade_acompanhantes) values (1, 'Pedro', 20);
+insert into guest (id, nome, quantidade_acompanhantes) values (2, 'Maria', 30);
+insert into guest (id, nome, quantidade_acompanhantes) values (3, 'Ricardo', 10);
